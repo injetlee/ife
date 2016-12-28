04caf9efb8e675edd2ce8b6a007d7a7e3949deac
injetlee <liyingjie26@hotmail.com>
2016-12-28 22:54:04 +0800

    task19 finish

--- task19/scripts/task19.js
+++ task19/scripts/task19.js
@@ -1,5 +1,7 @@
+var val,flag=false;
 var res = {
 	"arr":[],
+	"delay":10,
 	"left-in":function(){
 		var value = document.getElementById("input").value;
 		pattern = /^[0-9]+$/;
@@ -32,26 +34,55 @@ var res = {
 		res.arr = []
 		for(var i=0;i<50;i++){
 			var value = Math.floor(Math.random()*100)
-			console.log(value)
 			res.arr.push(value)
 			render()
 		}
 	},
 	"rank":function(){
-		valueList = res.arr;
-		console.log('cccc'+valueList)
-		count = 1;
-		while (count<50){
-			for(var i=0;i<res.arr.length;i++){
+		// var flag = false;
+		var delay = document.getElementById("speed").value;
+		console.log(delay)
+
+		if(delay){
+			res.delay = delay;
+		}
+		var valueList = res.arr;
+		var i=0,j=1;
+		if(flag==false && valueList.length>0){
+			var timer = setInterval(function(){
+				flag = true;
 				if(valueList[i]>valueList[i+1]){
 					temp = valueList[i];
 					valueList[i] = valueList[i+1];
 					valueList[i+1] = temp;
 				}
-			}
-			count += 1 ;
-			render()
+				render(i)
+				++i;
+				
+				if(i == res.arr.length-j){
+					i = 0;
+					++j;
+				}
+				if(j==res.arr.length){
+					flag = false;
+					clearInterval(timer)
+					render()
+				}
+			},res.delay)
+		}else if(flag==true){
+			alert("正在排序")
 		}
+		// while (count<50){
+		// 	for(var i=0;i<res.arr.length;i++){
+		// 		if(valueList[i]>valueList[i+1]){
+		// 			temp = valueList[i];
+		// 			valueList[i] = valueList[i+1];
+		// 			valueList[i+1] = temp;
+		// 		}
+		// 	}
+		// 	count += 1 ;
+		// 	render()
+		// }
 	}
 }
 
@@ -60,8 +91,7 @@ for(var i=0;i<getButton.length;i++){
 	getButton[i].onclick = res[getButton[i].id]
 }
 
-function render(){
-	console.log('bbv')
+function render(val){
 	var str=""
 	var getDisplay = document.getElementById("display-area");
 	if(res.arr.length>60){
@@ -71,6 +101,11 @@ function render(){
 		str += "<div>" + res.arr[i] + "</div>"
 	}
 	getDisplay.innerHTML = str;
+	var getVal = getDisplay.getElementsByTagName("div");
+	if(val>-1 && val<49){
+		getVal[val].style.background="green";
+		getVal[val+1].style.background="green"
+	}
 	var getDisplay = document.getElementById("display-area");
 	var getDiv = getDisplay.getElementsByTagName("div");
 	for(var i=0;i<getDiv.length;i++){
